f641c1823218e363fba2e8559fba8f5987a7aa38
Jingsong Xiong <jingsong.xiong@compass.com>
2022-12-18 11:54:04 +0800

    patch:add RPCServer

--- server.py
+++ server.py
@@ -1,29 +1,32 @@
 import struct
 from io import BytesIO
+import socket
+
 
 # struct的格式请参考：https://docs.python.org/zh-cn/dev/library/struct.html
+class InvalidOperation(Exception):
+    def __int__(self, message=None):
+        self.message = message or 'Invalid operation'
 
 
 # divide过程消息协议转换工具
 class DivideProtocol(object):
 
-    def _read_all(self,size):
-        if isinstance(self.connection,BytesIO):
+    def _read_all(self, size):
+        if isinstance(self.connection, BytesIO):
             buff = self.connection.read(size)
             return buff
         else:
             have = 0
             buff = b''
             while have < size:
-                chunk = self.connection.recv(size-have)
+                chunk = self.connection.recv(size - have)
                 buff += chunk
                 have += len(chunk)
 
                 if len(chunk) == 0:
                     raise EOFError()
             return buff
-
-
 
     def args_encode(self, num1, num2=1):
         """将原始请求的参数转换成二进制数据"""
@@ -82,8 +85,7 @@ class DivideProtocol(object):
         buff = self._read_all(1)
         have += 1
         param_seq = struct.unpack('!B', buff)[0]
-        print('param_seq---------------111111')
-        print(param_seq)
+
         # 处理参数值
         param_len = param_len_map[param_seq]
         param_fmt = param_fmt_map[param_seq]
@@ -99,8 +101,6 @@ class DivideProtocol(object):
         buff = self._read_all(1)
         have += 1
         param_seq = struct.unpack('!B', buff)[0]
-        print('param_seq---------------22222')
-        print(param_seq)
         # 处理参数值
         param_len = param_len_map[param_seq]
         param_fmt = param_fmt_map[param_seq]
@@ -112,26 +112,58 @@ class DivideProtocol(object):
 
         return args
 
+    def result_encode(self, result):
+        # 正常
+        if isinstance(result, float):
+            # 1个字节的序号 ｜ 4个字节的float结果
+            buff = struct.pack('!B', 1)
+            buff += struct.pack('!f', result)
+            return buff
+        # 异常
+        else:
+            buff = struct.pack('!B', 2)
+            buff += struct.pack('!I', len(result.message))
+            buff += result.message.encode()
+            return buff
+
+    def result_decode(self, connection):
+        self.connection = connection
+        buff = self._read_all(1)
+        result_type = struct.unpack('!B', buff)[0]
+
+        if result_type == 1:
+            buff = self._read_all(4)
+            return struct.unpack('!f', buff)[0]
+        else:
+            # 异常
+            buff = self._read_all(4)
+            length = struct.unpack('!I', buff)[0]
+            buff = self._read_all(length)
+            message = buff.decode()
+            return InvalidOperation(message)
+
+
 # 解读方法名
 class MethodProtocol(object):
-    def __init__(self,connection):
+    def __init__(self, connection):
         self.connection = connection
+
     def get_method_name(self):
         # 读取字符串长度
         buff = self._read_all(4)
-        length = struct.unpack('!I',buff)[0]
+        length = struct.unpack('!I', buff)[0]
         buff = self._read_all(length)
         return buff.decode()
 
-    def _read_all(self,size):
-        if isinstance(self.connection,BytesIO):
+    def _read_all(self, size):
+        if isinstance(self.connection, BytesIO):
             buff = self.connection.read(size)
             return buff
         else:
             have = 0
             buff = b''
             while have < size:
-                chunk = self.connection.recv(size-have)
+                chunk = self.connection.recv(size - have)
                 buff += chunk
                 have += len(chunk)
 
@@ -139,10 +171,39 @@ class MethodProtocol(object):
                     raise EOFError()
             return buff
 
+
+# 客户端建立网络连接
+class Channel(object):
+    def __init__(self, host, post):
+        self.host = host
+        self.post = post
+
+    def get_connection(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.post))
+        return sock
+
+
+class RPCServer(object):
+    def __init__(self, host, post):
+        self.host = host
+        self.post = post
+        # 创建socket 的工具对象
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # 设置socket 重用地址
+        sock.setsockopt(socket.SOL_SOCKET, socket.SOCK_STREAM, 1)
+        # 绑定地址
+        sock.bind((self.host, self.post))
+        self.sock = sock
+
+
+
+
 if __name__ == '__main__':
     # 构造消息体
     proto = DivideProtocol()
-    message = proto.args_encode(200,100)
+    message = proto.args_encode(200, 100)
 
     conn = BytesIO()
     conn.write(message)
@@ -154,4 +215,3 @@ if __name__ == '__main__':
     args = proto.args_decode(conn)
     print('args------------')
     print(args)
-
